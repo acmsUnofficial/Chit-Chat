f6b5adef51bd0fa0824888e182748c711126cc99
acmsUnofficial <47537678+acmsUnofficial@users.noreply.github.com>
2019-04-14 23:21:49 +0530

    Update SendingImage.java

--- java/SendingImage.java
+++ java/SendingImage.java
@@ -1,131 +1,61 @@
 protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
-            //if the request code is ok then we take the data uri of the image
+        //---FOR PICKING IMAGE FROM GALLERY ACTIVITY AND SENDING---
+        if(requestCode == GALLERY_PICK && resultCode == RESULT_OK){
+
+            //---GETTING IMAGE DATA IN FORM OF URI--
             Uri imageUri = data.getData();
-            //cropping the image taken from gallery to 1:1 ratio
-            CropImage.activity(imageUri)
-                    .setAspectRatio(1, 1)
-                    .start(this);
-        }
-//if the crop is done carefully then it is send below
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-//taking the cropped image uri into result uri
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            final String current_user_ref = "messages/"+mCurrentUserId+"/"+mChatUser;
+            final String chat_user_ref = "messages/"+ mChatUser +"/"+mCurrentUserId;
 
-            if (resultCode == RESULT_OK) {
+            DatabaseReference user_message_push = mRootReference.child("messages")
+                    .child(mCurrentUserId).child(mChatUser).push();
 
-                String currentUserId = mFirebaseUser.getUid();
+            final String push_id = user_message_push.getKey();
 
-                Uri resultUri = result.getUri();
+            //---PUSHING IMAGE INTO STORAGE---
+            final StorageReference filepath = mImageStorage.child("message_images").child(push_id+".jpg");
+            filepath.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
 
-                File thumbFilePath = new File(resultUri.getPath());
+                    if(task.isSuccessful()){
 
-               try {
-                   //--//
-                   Bitmap thumb_bitmap = new Compressor(this)
-                           .setMaxHeight(200)
-                           .setMaxWidth(200)
-                           .setQuality(75)
-                           .compressToBitmap(thumbFilePath);
+                        @SuppressWarnings("VisibleForTests")
+                        String download_url = filepath.getDownloadUrl().toString();
 
+                        Map messageMap = new HashMap();
+                        messageMap.put("message",download_url);
+                        messageMap.put("seen",false);
+                        messageMap.put("type","image");
+                        messageMap.put("time",ServerValue.TIMESTAMP);
+                        messageMap.put("from",mCurrentUserId);
 
-                   ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                   thumb_bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                   final byte[] thumb_bytes = baos.toByteArray();
-               }
+                        Map messageUserMap = new HashMap();
+                        messageUserMap.put(current_user_ref+"/"+push_id,messageMap);
+                        messageUserMap.put(chat_user_ref+"/"+push_id,messageMap);
 
-               catch(IOException e)
-            {
-                e.printStackTrace();
-            }
-                // getting the file path of the storage in firebase storage into to file path
-                final StorageReference filePath = mStorageReference.child("profile_image").child(currentUserId + ".jpg");
+                        mRootReference.updateChildren(messageUserMap, new DatabaseReference.CompletionListener(){
 
-                final StorageReference thumbFile1 = mStorageReference.child("profile_image").child("thumbnails").child(currentUserId + ".jpg");
-
-
-                filePath.putFile(resultUri).continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-                    @Override
-                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                        if (!task.isSuccessful()) {
-                            throw task.getException();
-                        }
-                        return filePath.getDownloadUrl();
-                        //getting the downlaod url
-                    }
-                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                    @Override
-                    public void onComplete(@NonNull final Task<Uri> task) {
-                        if (task.isSuccessful()) {
-
-                            Uri downUri = task.getResult();
-                            imageDownload = downUri.toString();
-
-
-                            final UploadTask uploadTask = thumbFile1.putBytes(thumb_bytes);
-                            uploadTask.addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception exception) {
-                                    // Handle unsuccessful uploads
+                            @Override
+                            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                if(databaseError != null){
+                                    Log.e("CHAT_ACTIVITY","Cannot add message to database");
                                 }
-                            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                                    Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-                                        @Override
-                                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                                            if (!task.isSuccessful()) {
-                                                throw task.getException();
-                                            }
-
-                                            // Continue with the task to get the download URL
-                                            return thumbFile1.getDownloadUrl();
-                                        }
-                                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Uri> task) {
-                                            if (task.isSuccessful()) {
-                                                Uri downloadUri = task.getResult();
-                                                String ThumbUrl = downloadUri.toString();
-
-                                                Map ImgData = new HashMap();
-                                                ImgData.put("image", imageDownload);
-                                                ImgData.put("thumb_image", ThumbUrl);
-                                               
-
-
-                                                mDatabaseReference.updateChildren(ImgData).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                    @Override
-                                                    public void onSuccess(Void aVoid) {
-                                                        if (aVoid != null) {
-                                                            Toast.makeText(getApplicationContext(), "picupdates in datavbase", Toast.LENGTH_SHORT).show();
-                                                        } else {
-                                                            Toast.makeText(getApplicationContext(), "Error in thumbnail", Toast.LENGTH_SHORT).show();
-                                                        }
-                                                    }
-                                                });
-
-
-                                            } else {
-
-
-                                            }
-                                        }
-                                    });
-
+                                else{
+                                    Toast.makeText(ChatActivity.this, "Message sent", Toast.LENGTH_SHORT).show();
+                                    mMessageView.setText("");
                                 }
-                            });
 
-
-                        }
+                            }
+                        });
                     }
-                });
+
+                }
+            });
 
 
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
-            }
         }
+
+    }
